4e3a324eae3e7b8af1cf04aa946afbfc9b537c01
Alexandru Zahar <zaharalex25@gmail.com>
2024-02-25 16:04:27 +0100

    Updated the embeddings script to support pdf read

--- embed.py
+++ embed.py
@@ -1,8 +1,13 @@
+import fitz  # Import PyMuPDF
 import ollama
-def read_file(file_path):
-    with open(file_path, 'r') as file:
-        return file.read()
-    
+
+def read_pdf(file_path):
+    # Open the PDF file
+    with fitz.open(file_path) as doc:
+        text = ""
+        for page in doc:
+            text += page.get_text()
+    return text
 
 def split_into_chunks(text, chunk_size):
     words = text.split()
@@ -10,21 +15,23 @@ def split_into_chunks(text, chunk_size):
     current_chunk = []
 
     for word in words:
-       current_chunk.append(word)
-       if len(current_chunk) >= chunk_size:
-           chunks.append(" ".join(current_chunk))
-           current_chunk = []
-
-       if current_chunk:
+        current_chunk.append(word)
+        if len(current_chunk) >= chunk_size:
             chunks.append(" ".join(current_chunk))
+            current_chunk = []
+    # Add the last chunk if it contains any words
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
     return chunks
 
-file_path = "./text_embed_example.txt"
-long_string = read_file(file_path)
+file_path = "./002-GINGER-THE-GIRAFFE-Free-Childrens-Book-By-Monkey-Pen.pdf"
+long_string = read_pdf(file_path)
 chunk_size = 500
 chunks = split_into_chunks(long_string, chunk_size)
 model = "nomic-embed-text"
-
+print(f"long_string: {long_string}")
+print(f"Number of chunks: {len(chunks)}")
 for i, chunk in enumerate(chunks, start=1):
     response = ollama.embeddings(model, prompt=chunk)
-    print(i)
+    print(f"Chunk {i} response:", response)
+
